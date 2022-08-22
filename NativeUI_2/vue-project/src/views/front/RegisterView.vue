ce2b5b96d26126bcb9d30b84d7755049c95fdf78
Parnell <anny4442@gmail.com>
2022-08-23 01:01:46 +0800

    前台驗證

--- NativeUI_2/vue-project/src/views/front/RegisterView.vue
+++ NativeUI_2/vue-project/src/views/front/RegisterView.vue
@@ -6,46 +6,18 @@
 
 
             <n-tabs default-value="signin" size="large" justify-content="space-evenly" animated>
-                <n-tab-pane name="signin" tab="登入會員">
 
-                    <!-- <n-form :model="formLogin" :rules="rulesLogin"> -->
-                    <n-form :model="formLogin" ref="formRef" :rules="rules" @submit.prevent='register'>
-                        <!-- @submit.prevent  提交後不刷新頁面 -->
-
-
-                        <!-- <n-form-item-row label="帳號" :rules="rules.account" path="account"> -->
-                        <n-form-item-row label="帳號" path="account">
-                            <n-input v-model:value="formLogin.account" placeholder="請輸入帳號" />
-                        </n-form-item-row>
-
-                        <h1>測試帳號 {{ formLogin.account }}</h1>
-                        <h1>測試密碼 {{ formLogin.password }}</h1>
-
-                        <!-- <n-form-item-row label="密碼" :rules="rules.password" path="password"> -->
-                        <n-form-item-row label="密碼" path="password">
-                            <n-input v-model:value="formLogin.password" placeholder="請輸入密碼" />
-                        </n-form-item-row>
-
-                        <n-button type="primary" @click="handleValidateClick" block secondary strong round
-                            attr-type="submit">
-                            登入
-                        </n-button>
-
-                    </n-form>
-
-
-                </n-tab-pane>
 
 
 
                 <n-tab-pane name="signup" tab="註冊">
 
-                    <!-- <n-form ref="formRef">
-                        <n-form-item-row label="姓名"  v-model:value="formValue.user.name">
-                            <n-input placeholder="請輸入使用者名稱" />
+                    <!-- <n-form :model="formRegister" ref="formRef" :rules="rules">
+                        <n-form-item-row label="姓名" path="name">
+                            <n-input v-model:value="formRegister.name" placeholder="請輸入使用者名稱" />
                         </n-form-item-row>
-                        <n-form-item-row label="帳號">
-                            <n-input placeholder="請輸入八到十五個英文或數字" />
+                        <n-form-item-row label="帳號" path="account">
+                            <n-input v-model:value="formRegister.account" placeholder="請輸入八到十五個英文或數字" />
                         </n-form-item-row>
                         <n-form-item-row label="密碼">
                             <n-input placeholder="請輸入八到十五個英文或數字" />
@@ -53,16 +25,41 @@
                         <n-form-item-row label="信箱">
                             <n-input placeholder="請輸入信箱" />
                         </n-form-item-row>
-                        <n-form-item-row label="連絡電話" v-model:value="formValue.phone">
-                            <n-input label="請輸入連絡電話" />
+                        <n-form-item-row label="連絡電話">
+                            <n-input v-model:value="formRegister.phone" label="請輸入連絡電話" />
                         </n-form-item-row>
                     </n-form> -->
+
+
+
+                    <!-- 用formLogin測試 -->
+                    <n-form :model="form" ref="formRef" :rules="rules">
+
+                        <n-form-item-row label="姓名" path="name">
+                            <n-input v-model:value="form.name" placeholder="請輸入使用者名稱" />
+                        </n-form-item-row>
+                        <n-form-item-row label="帳號" path="account">
+                            <n-input v-model:value="form.account" placeholder="請輸入八到十五個英文或數字" />
+                        </n-form-item-row>
+                        <n-form-item-row label="密碼" path="password">
+                            <n-input v-model:value="form.password" placeholder="請輸入八到十五個英文或數字" />
+                        </n-form-item-row>
+                        <n-form-item-row label="信箱" path="email">
+                            <n-input v-model:value="form.email" placeholder="請輸入信箱" />
+                        </n-form-item-row>
+                        <n-form-item-row label="連絡電話">
+                            <n-input v-model:value="form.phone" label="請輸入連絡電話" />
+                        </n-form-item-row>
+                    </n-form>
 
                     <n-button attr-type="submit" type="primary" block secondary strong round>
                         註冊
                     </n-button>
 
                 </n-tab-pane>
+
+
+
             </n-tabs>
         </n-card>
     </div>
@@ -95,67 +92,113 @@ window.$message = useMessage();
 
 
 
-reactive
-const formLogin = ref({
-    account: '',
-    password: ''
-})
+// reactive
+// const formLogin = ref({
+//     account: '',
+//     password: ''
+// })
 
-/* const formRegister = reactive({
+
+const form = ref({
+    //   login register 的 formValue合併
+    // 註冊登入按鈕事件分開寫
     account: '',
     password: '',
     email: '',
     phone: '',
     userName: ''
-}) */
+})
 
-// async-validator 套件的規則
+// 老師的檔案是reactive
+// const formRegister = ref({
+//     account: '',
+//     password: '',
+//     email: '',
+//     phone: '',
+//     userName: ''
+// })
+
+
+
+
+
+
 const rules = reactive({
+    // async-validator 套件的規則
     account: {
         required: true,
-        pattern: /^[A-Za-z0-9]+$/,
-        min: 5,
-        // validator執行優先度比前幾個高
-        validator(rule, value,) {
+        // pattern 怪怪的優先權會跑掉
+        // min max validator 優先權也怪怪的
+        // pattern: /^[A-Za-z0-9]+$/,
+        // min: 5,
+
+
+        // message 執行優先度比 validator 高
+
+
+        validator (rule, value,) {
             if (!value) {
-                return new Error("請填寫帳號")
-            }
-            else if (formLogin.value.account.length >= 15) {
+                return new Error("請輸入帳號")
+            } else if (form.value.account.length > 15) {
                 return new Error("帳號請在15字以內（包含15個字）")
-            } else if (formLogin.value.account.length <= 5) {
+            } else if (form.value.account.length < 5) {
+                console.log(form.value.account.length)
+                console.log(form.value.account)
                 return new Error("帳號請在5個字以上（包含5個字）")
-            }
-
-
+            } else if (!/^[A-Za-z0-9]+$/.test(value))
+                return new Error("請輸入半形英文數字，避免特殊符號")
         },
 
-        message: "帳號格式錯誤",
 
+        // message: "格式錯誤",
         trigger: ["input", "blur"]
     },
     password: {
         required: true,
-        message: "請輸入密碼",
+
+        validator (rule, value) {
+            if (!value) {
+                return new Error("請輸入密碼")
+            } else if (form.value.password.length > 15 || form.value.password.length < 5 || !/^[A-Za-z0-9]+$/.test(value))
+                return new Error("請輸入5~15個半形英文數字，避免特殊符號")
+        },
+        // message: "請輸入5~15字半形英文或數字",
         trigger: ["input", "blur"]
     },
     email: {
+        // type: 'email',
         required: true,
         // 去除空格之後在檢驗
-        transform(value) {
-            // do something
-            return value.trim()
+        // transform (value) {
+        //     // do something
+        //     return value.trim()
+        // },
+
+        validator (rule, value) {
+            if (!value) {
+                console.log(form.value.email)
+                return new Error("請輸入電子信箱")
+
+            }
+            else if (!isEmail(form.value.email))
+                return new Error("信箱格式錯誤")
         },
-        message: "請輸入電子郵件",
+        // message: "請輸入電子郵件",
+        trigger: ["input", "blur"]
+
     },
     phone: {
         type: String,
         required: true,
         pattern: /^[0-9]+$/,
         message: "請輸入電話號碼",
+        trigger: ["input", "blur"]
     },
     userName: {
         required: true,
         message: "請輸入用戶名稱",
+        trigger: ["input", "blur"]
+
     }
 })
 
